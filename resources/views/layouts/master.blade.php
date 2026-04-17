419015a246f6287363a0b24d4d55b710fba52ed0
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-17 11:31:05 +0200

    Rimuovi vite scss

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
     <title>@yield("titolo")</title>
 
     <!-- Includo gli assets compilati da Vite -->
-    <!-- @vite(['resources/sass/app.scss', "resources/js/app.js"]) -->
     @vite(['resources/js/app.js'])
 
 </head>
