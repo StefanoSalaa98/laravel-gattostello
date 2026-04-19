3abd8ab08e6af6e754d82be7719b12811bb5c9db
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-19 18:54:12 +0200

    Aggiungi build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path'; // <-- require path from node
 
 export default defineConfig({
+    base: '/build/',   //riga aggiunta per railway
     plugins: [
         laravel({
             // edit the first value of the array input to point to our new sass files and folder.
