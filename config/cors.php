7d13d94fae9b204d5291d15870626c724b95197b
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-19 21:16:05 +0200

    Modifica cors

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,17 @@
+<?php
 return [
-'allowed_origins' => [
-'https://gattostello.vercel.app'
-],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://gattostello.vercel.app'
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'supports_credentials' => false,
 ];
+
+?>
