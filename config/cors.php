7bd76a6dec40ba1ea1e8ac90f1d5a1cf4fbc56dc
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-19 21:04:01 +0200

    Aggiungi cors

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+return [
+'paths' => ['api/*'],
+'allowed_methods' => ['*'],
+'allowed_origins' => [
+'https://gattostello.vercel.app/'
+],
+'allowed_headers' => ['*'],
+'supports_credentials' => false,
+];
