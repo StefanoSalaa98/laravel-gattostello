73d741bbbb5a4f5d40684f68c9de8fc4f462b14a
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-20 11:16:32 +0200

    Modifica cors

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        '*'
-        // 'https://gattostello.vercel.app'
+        'https://gattostello.vercel.app'
     ],
 
     'allowed_headers' => ['*'],
