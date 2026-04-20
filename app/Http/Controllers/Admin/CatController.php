7014e7ba524033ec848b531da82164a3aa3bcba6
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-20 18:08:53 +0200

    Modifica percorso salvataggio immagine

--- app/Http/Controllers/Admin/CatController.php
+++ app/Http/Controllers/Admin/CatController.php
@@ -63,8 +63,7 @@ class CatController extends Controller
         $newCat->adottato = (bool) $data['adottato'];
         $newCat->prenotato = (bool) $data['prenotato'];
         if ($request->hasFile('image')) {
-            // $img_url = Storage::putFile("cats", $request->file("image"));
-            $img_url = Storage::putFile("cats", $request->file("image"), 'public');
+            $img_url = Storage::putFile("cats", $request->file("image"));
             $newCat->image = $img_url;
         }
         $newCat->save();
