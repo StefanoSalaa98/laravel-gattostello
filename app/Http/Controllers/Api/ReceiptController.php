7c647f2705b6e2c69aaf1924cf9f3dfc987af6bc
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-07-14 23:33:34 +0200

    test

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -12,44 +12,79 @@ class ReceiptController extends Controller
 {
     public function store(Request $request)
     {
-        dd(config('mail.to.address'));
-        // Validazione lato server (Inviolabile)
-        $validated = $request->validate([
-            'cognome' => 'required|string|max:255',
-            'nome' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'cf' => 'required|string|size:16|regex:/^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i',
-            'via' => 'required|string|max:255',
-            'civico' => 'required|string|max:10',
-            'citta' => 'required|string|max:255',
-            'cap' => 'required|string|size:5|regex:/^[0-9]{5}$/',
-            'importo' => 'nullable|numeric|min:0',
-            'messaggio' => 'nullable|string|max:1000'
-        ]);
-
-        // Salva nel Database usando il Modello Eloquent
-        $receiptRequest = ReceiptRequest::create($validated);
-
-        // Invia la mail all'indirizzo dell'associazione
-        // Mail::to('infobellissime@gattostello.it')->send(new ReceiptRequested($receiptRequest));
-        // Mail::to(config('mail.to.address'))
-        //     ->send(new ReceiptRequested($receiptRequest));
-
         try {
+
+            $validated = $request->validate([
+                'cognome' => 'required|string|max:255',
+                'nome' => 'required|string|max:255',
+                'email' => 'required|email|max:255',
+                'cf' => 'required|string|size:16|regex:/^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i',
+                'via' => 'required|string|max:255',
+                'civico' => 'required|string|max:10',
+                'citta' => 'required|string|max:255',
+                'cap' => 'required|string|size:5|regex:/^[0-9]{5}$/',
+                'importo' => 'nullable|numeric|min:0',
+                'messaggio' => 'nullable|string|max:1000'
+            ]);
+
+            $receiptRequest = ReceiptRequest::create($validated);
+
             Mail::to(config('mail.to.address'))
                 ->send(new ReceiptRequested($receiptRequest));
+
+            return response()->json([
+                'message' => 'OK'
+            ]);
+
         } catch (\Throwable $e) {
+
+            \Log::error($e);
+
             return response()->json([
                 'message' => $e->getMessage(),
-                'file' => $e->getFile(),
-                'line' => $e->getLine(),
             ], 500);
         }
-
-        // Rispondi a React con successo
-        return response()->json([
-            'message' => 'Richiesta salvata e mail inviata con successo!'
-        ], 201);
     }
+    // public function store(Request $request)
+    // {
+    //     dd(config('mail.to.address'));
+    //     // Validazione lato server (Inviolabile)
+    //     $validated = $request->validate([
+    //         'cognome' => 'required|string|max:255',
+    //         'nome' => 'required|string|max:255',
+    //         'email' => 'required|email|max:255',
+    //         'cf' => 'required|string|size:16|regex:/^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i',
+    //         'via' => 'required|string|max:255',
+    //         'civico' => 'required|string|max:10',
+    //         'citta' => 'required|string|max:255',
+    //         'cap' => 'required|string|size:5|regex:/^[0-9]{5}$/',
+    //         'importo' => 'nullable|numeric|min:0',
+    //         'messaggio' => 'nullable|string|max:1000'
+    //     ]);
+
+    //     // Salva nel Database usando il Modello Eloquent
+    //     $receiptRequest = ReceiptRequest::create($validated);
+
+    //     // Invia la mail all'indirizzo dell'associazione
+    //     // Mail::to('infobellissime@gattostello.it')->send(new ReceiptRequested($receiptRequest));
+    //     // Mail::to(config('mail.to.address'))
+    //     //     ->send(new ReceiptRequested($receiptRequest));
+
+    //     try {
+    //         Mail::to(config('mail.to.address'))
+    //             ->send(new ReceiptRequested($receiptRequest));
+    //     } catch (\Throwable $e) {
+    //         return response()->json([
+    //             'message' => $e->getMessage(),
+    //             'file' => $e->getFile(),
+    //             'line' => $e->getLine(),
+    //         ], 500);
+    //     }
+
+    //     // Rispondi a React con successo
+    //     return response()->json([
+    //         'message' => 'Richiesta salvata e mail inviata con successo!'
+    //     ], 201);
+    // }
 }
 
