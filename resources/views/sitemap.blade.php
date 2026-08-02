618da8fdb10385231af8304bd210d11c38abee27
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 13:18:26 +0200

    test

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,53 +1,7 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
-
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>https://gattostello.it/</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/chi-siamo</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/adotta</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/ex-ospiti</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/sostienici</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.9</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/unisciti</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/eventi</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/ricevuta</loc>
-        <changefreq>yearly</changefreq>
-        <priority>0.4</priority>
     </url>
 
 </urlset>
