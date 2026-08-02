39f400bb8e2bef842e48bf589dff8c4a95e7eaa6
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 14:06:23 +0200

    test

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cat;
-
 class SitemapController extends Controller
 {
     public function index()
     {
-        $cats = Cat::all();
-
-        return response()
-            ->view('sitemap', compact('cats'))
-            ->header('Content-Type', 'application/xml');
+        return "CONTROLLER OK";
     }
-
-
 }
