5ace1f1474c2b9f772bd49f8a369d2853be1fac7
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 13:57:26 +0200

    test

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,27 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cat;
-
 class SitemapController extends Controller
 {
     public function index()
     {
-        try {
-            $cats = Cat::all();
-
-            return response()->json([
-                "success" => true,
-                "count" => $cats->count(),
-                "cats" => $cats
-            ]);
-
-        } catch (\Exception $e) {
-
-            return response()->json([
-                "error" => $e->getMessage()
-            ], 500);
-
-        }
+        return response()->json([
+            "test" => "controller funziona"
+        ]);
     }
 }
