4aa17256827da38b6e21ec21d42445f319ed7fb3
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 13:35:47 +0200

    test

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,7 +1,53 @@
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>https://gattostello.it/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
     </url>
+
+    <url>
+        <loc>https://gattostello.it/chi-siamo</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+    <url>
+        <loc>https://gattostello.it/adotta</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://gattostello.it/ex-ospiti</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+    <url>
+        <loc>https://gattostello.it/sostienici</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+    </url>
+
+
+    {{-- Pagine dei singoli gatti --}}
+    @foreach($cats as $cat)
+
+        <url>
+            <loc>https://gattostello.it/adotta/{{ $cat->slug }}</loc>
+
+            @if($cat->updated_at)
+                <lastmod>{{ $cat->updated_at->toDateString() }}</lastmod>
+            @endif
+
+            <changefreq>weekly</changefreq>
+            <priority>0.9</priority>
+        </url>
+
+    @endforeach
+
 
 </urlset>
