ab2c86f1e6f60813f2d625d67191cbb5cadb6754
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 13:45:05 +0200

    test

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,49 +1,15 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+<?php
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+namespace App\Http\Controllers;
 
-    <url>
-        <loc>https://gattostello.it/</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-    </url>
+use App\Models\Cat;
 
-    <url>
-        <loc>https://gattostello.it/chi-siamo</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
+class SitemapController extends Controller
+{
+    public function index()
+    {
+        $cats = Cat::all();
 
-    <url>
-        <loc>https://gattostello.it/adotta</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/ex-ospiti</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://gattostello.it/sostienici</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.9</priority>
-    </url>
-
-
-    {{-- Pagine dei singoli gatti --}}
-    @foreach($cats as $cat)
-
-        <url>
-            <loc>https://gattostello.it/adotta/{{ $cat->slug }}</loc>
-
-            <changefreq>weekly</changefreq>
-            <priority>0.9</priority>
-        </url>
-
-    @endforeach
-
-
-</urlset>
+        return response()->json($cats);
+    }
+}
