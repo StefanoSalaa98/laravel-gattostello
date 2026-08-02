e0b26eadceb51a1fff8d6c73a614554e14c4427f
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-02 13:31:07 +0200

    test

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -1,19 +1,53 @@
-<?php
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 
-namespace App\Http\Controllers;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-use App\Models\Cat;
+    <url>
+        <loc>https://gattostello.it/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
 
-class SitemapController extends Controller
-{
-    public function index()
-    {
-        $cats = Cat::all();
+    <url>
+        <loc>https://gattostello.it/chi-siamo</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
 
-        return response()
-            ->view('sitemap', compact('cats'))
-            ->header('Content-Type', 'application/xml');
-    }
+    <url>
+        <loc>https://gattostello.it/adotta</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://gattostello.it/ex-ospiti</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+    <url>
+        <loc>https://gattostello.it/sostienici</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+    </url>
 
 
-}
+    {{-- Pagine dei singoli gatti --}}
+    @foreach($cats as $cat)
+
+        <url>
+            <loc>https://gattostello.it/adotta/{{ $cat->slug }}</loc>
+
+            @if($cat->updated_at)
+                <lastmod>{{ $cat->updated_at->toDateString() }}</lastmod>
+            @endif
+
+            <changefreq>weekly</changefreq>
+            <priority>0.9</priority>
+        </url>
+
+    @endforeach
+
+
+</urlset>
