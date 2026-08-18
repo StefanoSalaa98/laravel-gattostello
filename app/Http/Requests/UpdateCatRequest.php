827053d6d2e476220d17da8c671b2e3514fbfb40
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-08-18 19:01:42 +0200

    Aggiorna validazione

--- app/Http/Requests/UpdateCatRequest.php
+++ app/Http/Requests/UpdateCatRequest.php
@@ -25,7 +25,7 @@ class UpdateCatRequest extends FormRequest
 
         return [
             'name' => 'required|string|max:255',
-            'sex' => 'required|in:M,F',
+            'sex' => 'required|in:M,F,MF',
             'date_of_birth' => 'nullable|date_format:Y-m',
             'coat' => 'nullable|string|max:100',
             'info' => 'nullable|string',
@@ -39,7 +39,7 @@ class UpdateCatRequest extends FormRequest
     {
         return [
             'name.required' => 'Il nome è obbligatorio',
-            'sex.in' => 'Il sesso deve essere M o F',
+            'sex.in' => 'Il sesso deve essere M o F o MF',
             'date_of_birth.date_format' => 'La data di nascita deve essere nel formato YYYY-MM',
             'image.image' => 'Il file deve essere una immagine',
         ];
